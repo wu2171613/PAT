1052f9bc806b78dcaf7a98208eea3206ab283a3e
wu2171613 <wu2171613@gmail.com>
2017-10-22 11:43:44 +0800

    add PAT basic level practise 1056

---
+++ src/main/java/pat/level/basic/P1056.java
@@ -0,0 +1,7 @@
+package pat.level.basic;
+
+import java.util.Scanner;
+
+public class P1056 {
+
+}
---
+++ src/main/test/pat/level/basic/P1056Test.java
@@ -0,0 +1,12 @@
+package pat.level.basic;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+
+}
